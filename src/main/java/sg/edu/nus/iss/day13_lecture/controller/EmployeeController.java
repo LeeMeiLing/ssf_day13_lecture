2bce160a986fc413cef80082db882b605183addd
LeeMeiLing <leem0060@outlook.com>
2023-03-18 00:53:22 +0800

    added comment @RequestBody does not work for urlencoded form data

--- src/main/java/sg/edu/nus/iss/day13_lecture/controller/EmployeeController.java
+++ src/main/java/sg/edu/nus/iss/day13_lecture/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
@@ -74,8 +76,9 @@ public class EmployeeController {
     }
 
 
-    @PostMapping("/update")
+    @PostMapping(path={"/update"})
     public String updateEmployeeProcess(@Valid @ModelAttribute("employee") Employee emp, 
+    // public String updateEmployeeProcess(@Valid @RequestBody Employee emp, // @RequestBody cannot parse urlencoded form data
     BindingResult bindingresult, Model model){
 
         if(bindingresult.hasErrors()){
